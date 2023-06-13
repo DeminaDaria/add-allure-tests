f57ba04a691fde360fe370433f273ccccfdb61be
daria <darya.demina@simbirsoft.com>
2023-06-13 07:23:13 +0300

    add annotation steps test

---
+++ src/test/java/com/deminadarya/pages/WebSteps.java
@@ -0,0 +1,36 @@
+package com.deminadarya.pages;
+
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class WebSteps {
+
+    @Step("Открыть главную страницу")
+    public void openMainPage() {
+        open("https://github.com");
+    }
+
+    @Step("Выполнить поиск введённого наименования репозитория {repo}")
+    public void searchRepo(String repo) {
+        $(".header-search-input").setValue(repo).pressEnter();
+    }
+
+    @Step("Выполнить переход в репозиторий")
+    public void goToRepo() {
+        $("a[href$='/eroshenkoam/allure-example']").click();
+    }
+
+    @Step("Выполнить переход в раздел Issues")
+    public void switchToIssue() {
+        $("#issues-tab").click();
+    }
+
+    @Step("Поиск по тексту названия Issues {nameIssue}")
+    public void searchNameIssue(String nameIssue) {
+        $("#issue_81_link").shouldHave(text(nameIssue));
+    }
+
+}
--- src/test/java/com/deminadarya/tests/CheckNameRepoIssueTest.java
+++ src/test/java/com/deminadarya/tests/CheckNameRepoIssueTest.java
@@ -1,6 +1,7 @@
 package com.deminadarya.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.deminadarya.pages.WebSteps;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,31 +16,43 @@ public class CheckNameRepoIssueTest {
         Configuration.holdBrowserOpen = true;
     }
 
+    private final static String repo =  "eroshenkoam/allure-example";
+    private final static String nameIssue =  "issue_to_test_allure_report";
+    private WebSteps steps = new WebSteps();
+
     @Test
     public void shouldHaveIssueNameWithoutAllureReport() {
         open("https://github.com");
-        $(".header-search-input").setValue("eroshenkoam/allure-example").pressEnter();
+        $(".header-search-input").setValue(repo).pressEnter();
         $("a[href$='/eroshenkoam/allure-example']").click();
         $("#issues-tab").click();
-        $("#issue_81_link").shouldHave(text("issue_to_test_allure_report"));
+        $("#issue_81_link").shouldHave(text(nameIssue));
     }
 
     @Test
     public void shouldHaveIssueNameWithLambdaStep() {
-        step("Открыть главную страницу", () -> open("https://github.com"));
-        step("Выполнить поиск введённого наименования репозитория", () -> {
-            $(".header-search-input").setValue("eroshenkoam/allure-example").pressEnter();
-        });
-        step("Выполнить переход в репозиторий", () -> $("a[href$='/eroshenkoam/allure-example']").click());
-        step("Выполнить переход в раздел Issues", () -> $("#issues-tab").click());
-        step("Поиск по тексту названия Issues", () -> {
-            $("#issue_81_link").shouldHave(text("issue_to_test_allure_report"));
-        });
+        step("Открыть главную страницу",
+                () -> open("https://github.com"));
+        step("Выполнить поиск введённого наименования репозитория",
+                () -> {
+                    $(".header-search-input").setValue(repo).pressEnter();
+                });
+        step("Выполнить переход в репозиторий",
+                () -> $("a[href$='/eroshenkoam/allure-example']").click());
+        step("Выполнить переход в раздел Issues",
+                () -> $("#issues-tab").click());
+        step("Поиск по тексту названия Issues",
+                () -> {
+                    $("#issue_81_link").shouldHave(text(nameIssue));
+                });
     }
 
     @Test
     public void shouldHaveIssueNameWithAnnotationStep() {
-
+        steps.openMainPage();
+        steps.searchRepo(repo);
+        steps.goToRepo();
+        steps.switchToIssue();
+        steps.searchNameIssue(nameIssue);
     }
-
 }
